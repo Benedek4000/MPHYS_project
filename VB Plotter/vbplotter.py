8c582d291c17bb1584c00ec996a362288a1aa775
Benedek Kovács <kovacsbenedek4000@gmail.com>
2022-01-20 00:45:28 +0000

    vbplotter might be working

--- VB Plotter/vbplotter.py
+++ VB Plotter/vbplotter.py
@@ -36,17 +36,58 @@ def setupNumpyArray(fileLengthName, filenames, original_labels, memmapName): #se
         dataLength = sum(len(pycdf.CDF(i)[original_labels[0]][:]) for i in tqdm(filenames, desc='Setting Up Numpy Array'))
         with open(fileLengthName, 'w') as f:
             f.write(str(dataLength))
-    return np.memmap(memmapName, dtype=float, mode='w+', shape=(dataLength,7)) #[epoch, bgsm[x,y,z], v_gsm[x,y,z]]
+    return np.memmap(memmapName, dtype=float, mode='w+', shape=(dataLength,7)) #[bgsm[x,y,z], v_gsm[x,y,z]]
 
 def importFilenames(filePath, fileListName): #importing filenames
     return list((filePath+i[:-1]) for i in open(fileListName, 'r'))[:-1] #[:-1]s remove the \n from the end of each line and remove the txt filename from the list respectively
 
 def importData(data, MFIfilenames, SWEfilenames, original_MFI_lables, original_SWE_lables):
+    #generate date list
+    dates = []
+    for i in MFIfilenames: #assuming that for every SWE file, there is an MFI file
+        dates.append(i[31:39])
+    linkedFiles = {} #a dictionary
+    for currentDate in dates:
+        MFIname=''
+        SWEname=''
+        for currentMFI in MFIfilenames:
+            if currentDate in currentMFI:
+                MFIname = currentMFI
+        for currentSWE in SWEfilenames:
+            if currentDate in currentSWE:
+                SWEname = currentSWE
+        if MFIname != '' and SWEname != '':
+            linkedFiles[MFIname] = SWEname
+    #importing data into data day by day
     counter = 0
+    iterator = tqdm(linkedFiles.keys(), desc='Importing Data')
+    for currentKey in iterator:
+        currentValue = linkedFiles[currentKey]
+        with pycdf.CDF(currentKey) as currentMFIfile:
+            with pycdf.CDF(currentValue) as currentSWEfile:
+                saved_counter = counter
+                temp_counter = counter
+                for currentMFIEpoch in currentMFIfile[original_MFI_labels[1]][:]:
+                    data[counter][0] = 1000000*currentMFIEpoch.year+10000*currentMFIEpoch.month+100*currentMFIEpoch.day+currentMFIEpoch.hour
+                    counter += 1
+                for currentMFIline in currentMFIfile[original_MFI_labels[0]][:]:
+                    for i in range(3):
+                        data[temp_counter][i+1] = currentMFIline[i]
+                        temp_counter += 1
+                swedata = {}
+                for index, currentSWEEpoch in enumerate(currentSWEfile[original_SWE_labels[1]][:]):
+                    swedata[str(1000000*currentSWEEpoch.year+10000*currentSWEEpoch.month+100*currentSWEEpoch.day+currentSWEEpoch.hour)[:10]] = currentSWEfile[original_SWE_labels[0]][index]
+                currentSWEEpoch = currentSWEfile[original_SWE_labels[1]][0]
+                swedata[str(1000000*(currentSWEEpoch.year+1)+10000*currentSWEEpoch.month+100*currentSWEEpoch.day+currentSWEEpoch.hour)[:10]] = [None, None, None]
+                for i in range(saved_counter, counter, 1):
+                    for j in range(3):
+                        data[i][j+4] = swedata[str(data[i][0])[:10]][j]
+    #OLD VERSION STARTS HERE
+    """counter = 0
     for currentFileName in tqdm(MFIfilenames, desc='Importing MFI Data'):
         with pycdf.CDF(currentFileName) as currentFile:
             saved_counter = counter
-            for currentEpoch in currentFile[original_MFI_lables[1]][:]:
+            for currentEpoch in currentFile[original_MFI_labels[1]][:]:
                 data[counter][0] = 1000000*currentEpoch.year+10000*currentEpoch.month+100*currentEpoch.day+currentEpoch.hour
                 counter += 1
             for currentLine in currentFile[original_MFI_labels[0]][:]:
@@ -70,7 +111,7 @@ def importData(data, MFIfilenames, SWEfilenames, original_MFI_lables, original_S
                 else:
                     data[index][4] = None
                     data[index][5] = None
-                    data[index][6] = None
+                    data[index][6] = None"""
     return data
 
 def fixData(data): #replaces too high and too low values with None
@@ -88,7 +129,7 @@ def plotData(data, save, histogramFileName):
         for j in data:
             xaxis.append(j[i+4])
             yaxis.append(j[i+1])
-        ax.scatter(xaxis,yaxis, s=5)
+        ax.scatter(xaxis,yaxis, s=2)
     if save:
         plt.savefig(histogramFileName, dpi=100)
     else:
@@ -107,6 +148,5 @@ def main():
             currentline=currentline+str(round(j,2))+';'
         print(currentline)"""
     plotData(data, constants.save, constants.histogramFileName)
-            
 
 main()

9daa2a8e27d1b2e557b010514c02c2e8342a44f0
Benedek Kovács <kovacsbenedek4000@gmail.com>
2022-02-04 15:55:22 +0000

    extremeCalc import combined cdf file

--- extremeFuncCalc/extremeFC.py
+++ extremeFuncCalc/extremeFC.py
@@ -1,2 +1,25 @@
-
+from spacepy import pycdf
+from tqdm import tqdm
+from tqdm import trange
+import matplotlib.pyplot as plt
+import numpy as np
+import os.path as path
+import os
+import math
 import extreme_constants as constants
+
+def importFile(combinedFileName, labels):
+    with pycdf.CDF(combinedFileName) as fileData:
+        bin_structure = fileData[labels[0]][:]
+        no_of_entries = fileData[labels[1]][:]
+        bin_contents = []
+        for i in range(9):
+            bin_contents.append(fileData[labels[i+2]][:])
+        plotLabels = fileData[labels[11]][:]
+        mean = fileData[labels[12]][:]
+        std_dev = fileData[labels[13]][:]
+    return bin_structure, no_of_entries, bin_contents, mean, std_dev, plotLabels
+def main(lPath, combinedFileName, labels, sigmaCutoff, save, plotFileName):
+    bin_structure, no_of_entries, bin_contents, mean, std_dev, plotLabels = importFile(combinedFileName, labels)
+
+main(constants.lPath, constants.combinedFileName, constants.labels, constants.sigmaCutoff, constants.save, constants.plotFileName)
--- extremeFuncCalc/extreme_constants.py
+++ extremeFuncCalc/extreme_constants.py
@@ -1,1 +1,8 @@
 import os.path as path
+
+lPath = '/home/MPHYS/Data/'
+combinedFileName = path.join(lPath, 'combinedMFI.cdf')
+labels = ['BGSM_structure', 'BGSM_no_of_entries', 'BGSM_X_min', 'BGSM_Y_min', 'BGSM_Z_min', 'BGSM_X_int', 'BGSM_Y_int', 'BGSM_Z_int', 'BGSM_X_max', 'BGSM_Y_max', 'BGSM_Z_max', 'labels', 'mean', 'std_dev']
+sigmaCutoff=3
+save = False #True to save the plot, False to show the plot
+plotFileName = path.join(lPath, 'extremePlotFull.png')

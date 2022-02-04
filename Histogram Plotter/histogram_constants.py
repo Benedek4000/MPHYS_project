0e957963d2a1ed50cbc148d66acb30e6447b9450
Benedek Kovács <kovacsbenedek4000@gmail.com>
2022-02-04 15:27:49 +0000

    3,4 sigma shown on plots

--- Histogram Plotter/histogram_constants.py
+++ Histogram Plotter/histogram_constants.py
@@ -1,14 +1,15 @@
 import os.path as path
 
 lPath = '/home/MPHYS/Data/'
-MFIPath = path.join(lPath, 'MFI/')
+MFIPath = path.join(lPath, 'SWE/')
 MFIfileListName = path.join(MFIPath,'fileNames.txt')
 MFIfileLengthName = path.join(MFIPath, 'fileLength.txt')
-MFIcombinedFileName = path.join(lPath, 'combinedMFI.cdf')
+MFIcombinedFileName = path.join(lPath, 'combinedSWE.cdf')
 MFImemmapFileName = path.join(MFIPath, 'data.dat')
 solarActivityFileName = path.join(lPath, 'solarActivity.cdf')
 MFI_labels = ['V_GSM', 'Epoch', 'label_V_GSM']
-labels = ['BGSM_structure', 'BGSM_no_of_entries', 'BGSM_X_min', 'BGSM_Y_min', 'BGSM_Z_min', 'BGSM_X_int', 'BGSM_Y_int', 'BGSM_Z_int', 'BGSM_X_max', 'BGSM_Y_max', 'BGSM_Z_max', 'labels', 'mean', 'std_dev']
-bin_structure = [-50, 0.2, 100] #[bin1 lower limit, bin width, number of bins]
+labels = ['VGSM_structure', 'VGSM_no_of_entries', 'VGSM_X_min', 'VGSM_Y_min', 'VGSM_Z_min', 'VGSM_X_int', 'VGSM_Y_int', 'VGSM_Z_int', 'VGSM_X_max', 'VGSM_Y_max', 'VGSM_Z_max', 'labels', 'mean', 'std_dev']
+#bin_structure = [-50, 0.2, 500] #[bin1 lower limit, bin width, number of bins] FOR MFI
+bin_structure = [-1200, 1, 1600] #[bin1 lower limit, bin width, number of bins] FOR SWE
 save = False #True to save the plot, False to show the plot
 histogramFileName = path.join(lPath, 'histogramFull.png')

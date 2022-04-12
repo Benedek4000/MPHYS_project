3c4f524ca0a6bb75c2d90b0b3a0664c2beda59b4
Benedek Kovács <kovacsbenedek4000@gmail.com>
2022-04-12 18:21:39 +0100

    code calculated averaged return period/level

--- returnLevelCalc/returnCalc.py
+++ returnLevelCalc/returnCalc.py
@@ -9,11 +9,10 @@ SWE_parameter_file=path.join(lPath, 'SWEparameters.cdf')
 P_parameter_file=path.join(lPath, 'Pparameters.cdf')
 
 dataset=sys.argv[1]
-activity=sys.argv[2]
-coordinate=sys.argv[3]
-minmax=sys.argv[4]
-levper=sys.argv[5]
-value=float(sys.argv[6])
+coordinate=sys.argv[2]
+minmax=sys.argv[3]
+levper=sys.argv[4]
+value=float(sys.argv[5])
 
 if dataset=='MFI':
     p_file=MFI_parameter_file
@@ -21,13 +20,6 @@ elif dataset=='SWE':
     p_file=SWE_parameter_file
 elif dataset=='P':
     p_file=P_parameter_file
-
-if activity=='min':
-    activity=0
-elif activity=='int':
-    activity=1
-elif activity=='max':
-    activity=2
 
 if coordinate=='x':
     coordinate=0
@@ -44,20 +36,28 @@ elif minmax=='max':
 with pycdf.CDF(p_file) as parameter_file:
     parameters = parameter_file['parameters'][:]
     
-    if dataset=='P':
-        [xi, mu, sigma] = parameters[activity][minmax]
-    else:
-        [xi, mu, sigma] = parameters[activity][coordinate][minmax]
+    results=[]
+
+    for activity in [0,1,2]:
+        if dataset=='P':
+            [xi, mu, sigma] = parameters[activity][minmax]
+        else:
+            [xi, mu, sigma] = parameters[activity][coordinate][minmax]
     
-    if levper=='period':
-        if minmax==0: #minima
-            result=-mu+sigma/xi*(1-(-math.log(1-1/(365*value)))**(-xi))
-        elif minmax==0: #maxima
-            result=mu-sigma/xi*(1-(-math.log(1-1/(365*value)))**(-xi))
+        if levper=='period':
+            if minmax==0: #minima
+                results.append(-mu+sigma/xi*(1-(-math.log(1-1/(365*value)))**(-xi)))
+            elif minmax==0: #maxima
+                results.append(mu-sigma/xi*(1-(-math.log(1-1/(365*value)))**(-xi)))
+        elif levper=='level':
+            if minmax==0: #minima
+                results.append((1/365)/(1-math.e**(-(1+xi/sigma*(-value-mu))**(-1/xi))))
+            elif minmax==1: #maxima
+                results.append((1/365)/(1-math.e**(-(1+xi/sigma*(value-mu))**(-1/xi))))
+
+    if levper=='period': #calculate return level
+        result=(results[0]+results[1]+results[2])/3
         print('Return Level = '+str(result)+" nT or km/s or nPa")
-    elif levper=='level':
-        if minmax==0: #minima
-            result=(1/365)/(1-math.e**(-(1+xi/sigma*(-value-mu))**(-1/xi)))
-        elif minmax==1: #maxima
-            result=(1/365)/(1-math.e**(-(1+xi/sigma*(value-mu))**(-1/xi)))
+    elif levper=='level': #calculate return period
+        result=3/(1/results[0]+1/results[1]+1/results[2])
         print('Return Period = '+str(result)+" years")
